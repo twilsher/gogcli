3930a6fc889700e2a300c3b92d1b4cf171e20dbb
Thomas Wilsher <twilsher@gmail.com>
2026-05-06 09:21:21 +0200

    fix(cli): print parser construction errors

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -109,6 +109,7 @@ func Execute(args []string) (err error) {
 
 	parser, cli, err := newParser(helpDescription())
 	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, errfmt.Format(err))
 		return err
 	}
 
